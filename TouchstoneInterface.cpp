793065d1263b38fd860f669656e798e85d988481
Carson Ray <113852115+carsonray@users.noreply.github.com>
2025-02-16 15:30:08 -0500

    Add project files.

---
+++ TouchstoneInterface.cpp
@@ -0,0 +1,12 @@
+﻿// TouchstoneInterface.cpp : Defines the entry point for the application.
+//
+
+#include "TouchstoneInterface.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
