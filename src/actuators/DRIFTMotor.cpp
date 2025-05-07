8813ec4a506f0c19138e73ceede5344fc5f91dc7
Carson Ray <113852115+carsonray@users.noreply.github.com>
2025-05-07 13:37:41 -0400

    Fixed weird sign issue

--- src/actuators/DRIFTMotor.cpp
+++ src/actuators/DRIFTMotor.cpp
@@ -123,7 +123,7 @@ void DRIFTMotor::setForceTarget(double force) {
   setMode(FORCE);
   mutex.lock();
   if (force < 0) {
-    separationTarget = spoolOffset + -force/unitsPerRadian;
+    separationTarget = spoolOffset - force/unitsPerRadian;
   } else {
 	//If force is zero, no need to be right on the cusp of the tortional spring
     separationTarget = minSep;
